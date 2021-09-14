cd225a4304a61aa93f4732a48392dba3be02b933
Ian Marshall <imarshall14@gmail.com>
2021-09-14 13:13:44 -0400

    update meeting migration minutes_duration field to integer

--- db/migrate/20210914170821_create_meetings.rb
+++ db/migrate/20210914170821_create_meetings.rb
@@ -2,7 +2,7 @@ class CreateMeetings < ActiveRecord::Migration[6.1]
   def change
     create_table :meetings do |t|
       t.string :title, default: "New Meeting"
-      t.int :minutes_duration, default: 30
+      t.integer :minutes_duration, default: 30
       t.datetime :start_time, null: false, default => Datetime.now
       t.float :cost, default: 0
 
