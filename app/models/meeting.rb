11db21a04af0c5bfe2796e5bd692cf9b4c897adc
Ian Marshall <imarshall14@gmail.com>
2021-09-16 18:10:18 -0400

    add #total_cost and #current_cost instance methods to meeting model

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,6 +2,18 @@ class Meeting < ApplicationRecord
     has_many :users, through: :invitations
     validates :title, length: {minimum: 5}
 
-    def 
+    def total_cost
+        attendees = self.users
+        attendees.map { |a| a.rate*self.minutes_duration}
+        attendees.sum
+    end
+
+    def current_cost
+        invitations = self.invitations
+        invitations.each do |i|
+            i.attenance ? i.user.rate*self.minutes_duration : 0
+        end
+        invitations.sum
+    end
 
 end
