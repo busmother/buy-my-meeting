c5cf739e5e88a3d59854cea4f2621d6ee85aade9
Ian Marshall <imarshall14@gmail.com>
2021-09-16 16:37:48 -0400

    add namespacedroutes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :invitations
+        resources :meetings
+      end
+    end
+  end
+
+
 end
